565e0c4be484fb81ab0edee5754c98997e47a68a
Anonym Elev <anonymelev1234@gmail.com>
2019-11-04 00:41:52 +0100

    Change backend key

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ provider "heroku" {}
 terraform {
   backend "s3" {
     bucket = "pgr301bucket"
-    key    = "examen.pamen/terraform.tfstate"
+    key    = "anonymelev/terraform.tfstate"
     region = "eu-north-1"
   }
 }
