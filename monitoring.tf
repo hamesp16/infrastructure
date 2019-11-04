dda8f745c78d0ecc27c790f9225fd796b714304c
Anonym Elev <anonymelev1234@gmail.com>
2019-11-05 00:34:25 +0100

    Change statuscake website_url for testing

--- monitoring.tf
+++ monitoring.tf
@@ -1,6 +1,6 @@
 resource "statuscake_test" "ci" {
   website_name = "app-ci"
-  website_url = "${heroku_app.ci.web_url}"
+  website_url = "${heroku_app.ci.web_url}devices"
   test_type = "HTTP"
   check_rate = "300"
   contact_group = ["Default Contact Group"]
@@ -8,7 +8,7 @@ resource "statuscake_test" "ci" {
 
 resource "statuscake_test" "stage" {
   website_name = "app-stage"
-  website_url = "${heroku_app.staging.web_url}"
+  website_url = "${heroku_app.staging.web_url}devices"
   test_type = "HTTP"
   check_rate = "300"
   contact_group = ["Default Contact Group"]
@@ -16,7 +16,7 @@ resource "statuscake_test" "stage" {
 
 resource "statuscake_test" "prod" {
   website_name = "app-prod"
-  website_url = "${heroku_app.production.web_url}"
+  website_url = "${heroku_app.production.web_url}devices"
   test_type = "HTTP"
   check_rate = "300"
   contact_group = ["Default Contact Group"]
